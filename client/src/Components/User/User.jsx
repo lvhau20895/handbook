abdb0960318877af7785f2132b2a1089f27d4822
lvh20895 <levanhau12321@gmail.com>
2023-06-09 19:30:42 +0700

    fix navbar

--- client/src/Components/User/User.jsx
+++ client/src/Components/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "Slices/userSlice";
 import { Link } from "react-router-dom";
@@ -8,77 +8,81 @@ import { MdLiveHelp, MdMeetingRoom } from "react-icons/md";
 import style from "./user.module.scss";
 
 const User = () => {
-    const { user } = useSelector((state) => state.user);
-    const dispatch = useDispatch();
+	const [dropdown, setDropdown] = useState(false);
+	const { user } = useSelector(state => state.user);
+	const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getUser());
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(getUser());
+	}, [dispatch]);
 
-    const option = [
-        { icon: <FaUser />, title: "Profile" },
-        { icon: <AiFillSetting />, title: "Option" },
-        { icon: <MdLiveHelp />, title: "Help" },
-        { icon: <MdMeetingRoom />, title: "Logout" },
-    ];
+	const option = [
+		{ icon: <FaUser />, title: "Profile" },
+		{ icon: <AiFillSetting />, title: "Option" },
+		{ icon: <MdLiveHelp />, title: "Help" },
+		{ icon: <MdMeetingRoom />, title: "Logout" }
+	];
 
-    const { profile } = user;
+	const { profile } = user;
 
-    return (
-        <div className={style.user}>
-            <button className={style.avatar}>
-                <img
-                    src={
-                        profile?.avatar
-                            ? profile.avatar
-                            : "/images/avatar/default.png"
-                    }
-                    alt="avatar"
-                />
-            </button>
+	return (
+		<div className={style.user}>
+			<button
+				className={style.avatar}
+				onClick={() => setDropdown(!dropdown)}
+			>
+				<img
+					src={
+						profile?.avatar
+							? profile.avatar
+							: "/images/avatar/default.png"
+					}
+					alt="avatar"
+				/>
+			</button>
 
-            <div className={style.dropdown}>
-                <div className={style.option}>
-                    {user.role === "admin" && (
-                        <Link className={style.link}>
-                            <p className={style.icon}>
-                                <FaUserSecret />
-                            </p>
-                            <p className={style.title}>Admin</p>
-                        </Link>
-                    )}
-                    {option.map((item, index) => {
-                        return (
-                            <Link key={index} className={style.link}>
-                                <p className={style.icon}>{item.icon}</p>
-                                <p className={style.title}>{item.title}</p>
-                            </Link>
-                        );
-                    })}
-                    <div
-                        style={{
-                            height:
-                                user.role === "admin"
-                                    ? `calc(100% / ${option.length + 1})`
-                                    : `calc(100% / ${option.length})`,
-                        }}
-                        className={style.animate}
-                    ></div>
-                </div>
-            </div>
+			<div className={`${style.dropdown} ${dropdown ? style.show : ""}`}>
+				<div className={style.option}>
+					{user.role === "admin" && (
+						<Link className={style.link}>
+							<p className={style.icon}>
+								<FaUserSecret />
+							</p>
+							<p className={style.title}>Admin</p>
+						</Link>
+					)}
+					{option.map((item, index) => {
+						return (
+							<Link key={index} className={style.link}>
+								<p className={style.icon}>{item.icon}</p>
+								<p className={style.title}>{item.title}</p>
+							</Link>
+						);
+					})}
+					<div
+						style={{
+							height:
+								user.role === "admin"
+									? `calc(100% / ${option.length + 1})`
+									: `calc(100% / ${option.length})`
+						}}
+						className={style.animate}
+					></div>
+				</div>
+			</div>
 
-            <div className={style.info}>
-                <p className={style.name}>
-                    {profile?.nickname ? profile.nickname : user.username}
-                </p>
-                <p className={style.coin}>
-                    {profile?.coin
-                        ? Number(profile.coin).toLocaleString()
-                        : 0 + ".00$"}
-                </p>
-            </div>
-        </div>
-    );
+			<div className={style.info}>
+				<p className={style.name}>
+					{profile?.nickname ? profile.nickname : user.username}
+				</p>
+				<p className={style.coin}>
+					{profile?.coin
+						? Number(profile.coin).toLocaleString()
+						: 0 + ".00$"}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default User;
