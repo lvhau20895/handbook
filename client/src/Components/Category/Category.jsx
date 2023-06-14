84772b0f7726d8d5d39e8ec7a42adb6e24e7a1c9
lvh20895 <levanhau12321@gmail.com>
2023-06-14 17:59:19 +0700

    add animate category

--- client/src/Components/Category/Category.jsx
+++ client/src/Components/Category/Category.jsx
@@ -10,12 +10,13 @@ import {
 	FcTemplate,
 	FcVoicePresentation
 } from "react-icons/fc";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronUp } from "react-icons/fa";
+import { BiDotsHorizontalRounded } from "react-icons/bi";
 import style from "./category.module.scss";
 
 const Category = () => {
 	const [hovered, setHovered] = useState(null);
-	const [viewMore, setViewMore] = useState(false);
+	const [more, setMore] = useState(false);
 
 	const category = [
 		{ icon: <FcNews />, name: "News Feed" },
@@ -31,14 +32,34 @@ const Category = () => {
 	return (
 		<div className={style.category}>
 			<div className={style.menu}>
-				{category
-					.slice(0, viewMore ? category.length : 6)
-					.map((item, index) => {
+				{category.slice(0, 6).map((item, index) => {
+					return (
+						<Link
+							key={index}
+							className={style.link}
+							onMouseEnter={() => setHovered(index)}
+							onMouseLeave={() => setHovered(null)}
+						>
+							<p className={style.icon}>{item.icon}</p>
+							<p className={style.name}>{item.name}</p>
+						</Link>
+					);
+				})}
+
+				<div
+					style={{
+						height: more
+							? `calc(45px * ${category.slice(6).length})`
+							: 0
+					}}
+					className={`${style.extra} ${more ? style.show : ""}`}
+				>
+					{category.slice(6).map((item, index) => {
 						return (
 							<Link
 								key={index}
 								className={style.link}
-								onMouseEnter={() => setHovered(index)}
+								onMouseEnter={() => setHovered(index + 6)}
 								onMouseLeave={() => setHovered(null)}
 							>
 								<p className={style.icon}>{item.icon}</p>
@@ -46,14 +67,13 @@ const Category = () => {
 							</Link>
 						);
 					})}
+				</div>
 
 				<div
 					style={{
-						height: `calc(100% / ${
-							viewMore ? category.length : 6
-						})`,
+						height: `calc(100% / ${more ? category.length : 6})`,
 						top: `calc(100% / ${
-							viewMore ? category.length : 6
+							more ? category.length : 6
 						} * ${hovered})`
 					}}
 					className={`${style.animate} ${
@@ -63,13 +83,11 @@ const Category = () => {
 			</div>
 
 			<div className={style.more}>
-				<button onClick={() => setViewMore(!viewMore)}>
+				<button onClick={() => setMore(!more)}>
 					<p className={style.chevron}>
-						{!viewMore ? <FaChevronDown /> : <FaChevronUp />}
+						{more ? <FaChevronUp /> : <BiDotsHorizontalRounded />}
 					</p>
-					<p className={style.text}>
-						{!viewMore ? "View more" : "Hide"}
-					</p>
+					<p className={style.text}>{!more ? "More" : "Hide"}</p>
 				</button>
 			</div>
 		</div>
