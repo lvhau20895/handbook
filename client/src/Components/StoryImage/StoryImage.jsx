1083f32bc5bb496b6fc8091ca4ac005e0e199c88
lvh20895 <levanhau12321@gmail.com>
2023-06-28 11:36:06 +0700

    fix upload

--- client/src/Components/StoryImage/StoryImage.jsx
+++ client/src/Components/StoryImage/StoryImage.jsx
@@ -17,8 +17,14 @@ const StoryImage = () => {
 	const [imagePreview, setImagePreview] = useState("");
 	const [rotateImage, setRotateImage] = useState(0);
 	const [zoomLevel, setZoomLevel] = useState(1);
+	const [background, setBackground] = useState("white");
+	const [color, setColor] = useState("black");
+	const [contentPosition, setContentPosition] = useState({ x: 0, y: 0 });
+	const [isDragging, setIsDragging] = useState(false);
 
 	const emojiRef = useRef();
+	const containerRef = useRef();
+	const boxRef = useRef();
 
 	useCheckOutside(emojiRef, () => setShowEmoji(false));
 
@@ -65,6 +71,27 @@ const StoryImage = () => {
 		setZoomLevel(value);
 	};
 
+	const handleContentMouseDown = event => {
+		setIsDragging(true);
+		const offsetX = event.clientX - contentPosition.x;
+		const offsetY = event.clientY - contentPosition.y;
+		setContentPosition({ x: offsetX, y: offsetY });
+	};
+
+	const backgrounds = ["white", "black"];
+	const colors = [
+		"white",
+		"black",
+		"brown",
+		"red",
+		"green",
+		"darkblue",
+		"darkcyan",
+		"yellow",
+		"violet",
+		"pink"
+	];
+
 	return (
 		<div className={style.storyImage}>
 			<div className={style.option}>
@@ -95,6 +122,44 @@ const StoryImage = () => {
 							</div>
 						</div>
 					</div>
+
+					<div className={style.background}>
+						<p className={style.title}>Background</p>
+						<div className={style.wrap}>
+							{backgrounds.map((bg, i) => {
+								return (
+									<span
+										key={i}
+										style={{ background: bg }}
+										className={`${
+											bg === background
+												? style.active
+												: ""
+										}`}
+										onClick={() => setBackground(bg)}
+									></span>
+								);
+							})}
+						</div>
+					</div>
+
+					<div className={style.color}>
+						<p className={style.title}>Color</p>
+						<div className={style.wrap}>
+							{colors.map((clr, i) => {
+								return (
+									<span
+										key={i}
+										style={{ background: clr }}
+										className={`${
+											clr === color ? style.active : ""
+										}`}
+										onClick={() => setColor(clr)}
+									></span>
+								);
+							})}
+						</div>
+					</div>
 				</div>
 
 				<button className={style.up}>Up Story</button>
@@ -121,6 +186,19 @@ const StoryImage = () => {
 											alt="preview"
 										/>
 									</div>
+
+									<p
+										style={{
+											background,
+											color
+										}}
+										className={`${style.text} ${
+											value ? style.show : ""
+										}`}
+										onMouseDown={handleContentMouseDown}
+									>
+										{value}
+									</p>
 
 									<div className={style.setting}>
 										<label htmlFor="image">
@@ -158,7 +236,7 @@ const StoryImage = () => {
 										<AiOutlineCloudUpload />
 									</p>
 									<p className={style.desc}>
-										Choose a file or darg it here
+										Choose a file or drag it here
 									</p>
 								</label>
 							</div>
@@ -178,3 +256,84 @@ const StoryImage = () => {
 };
 
 export default StoryImage;
+
+// import React, { useEffect, useRef } from "react";
+// import "./styles.css";
+
+// function App() {
+//   const containerRef = useRef(null);
+//   const boxRef = useRef(null);
+
+//   const isClicked = useRef(false);
+
+//   const coords = useRef({
+//     startX: 0,
+//     startY: 0,
+//     lastX: 0,
+//     lastY: 0
+//   });
+
+//   useEffect(() => {
+//     if (!boxRef.current || !containerRef.current) return;
+
+//     const box = boxRef.current;
+//     const container = containerRef.current;
+
+//     const onMouseDown = (e) => {
+//       isClicked.current = true;
+//       coords.current.startX = e.clientX;
+//       coords.current.startY = e.clientY;
+//     };
+
+//     const onMouseUp = (e) => {
+//       isClicked.current = false;
+//       coords.current.lastX = box.offsetLeft;
+//       coords.current.lastY = box.offsetTop;
+//     };
+
+//     const onMouseMove = (e) => {
+//       if (!isClicked.current) return;
+
+//       const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+//       const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+//       const boxWidth = box.offsetWidth;
+//       const boxHeight = box.offsetHeight;
+//       const containerWidth = container.offsetWidth;
+//       const containerHeight = container.offsetHeight;
+
+//       const minX = 0;
+//       const maxX = containerWidth - boxWidth;
+//       const minY = 0;
+//       const maxY = containerHeight - boxHeight;
+
+//       const constrainedX = Math.max(minX, Math.min(nextX, maxX));
+//       const constrainedY = Math.max(minY, Math.min(nextY, maxY));
+
+//       box.style.top = `${constrainedY}px`;
+//       box.style.left = `${constrainedX}px`;
+//     };
+
+//     document.addEventListener("mousedown", onMouseDown);
+//     document.addEventListener("mouseup", onMouseUp);
+//     document.addEventListener("mousemove", onMouseMove);
+
+//     const cleanup = () => {
+//       document.removeEventListener("mousedown", onMouseDown);
+//       document.removeEventListener("mouseup", onMouseUp);
+//       document.removeEventListener("mousemove", onMouseMove);
+//     };
+
+//     return cleanup;
+//   }, []);
+
+//   return (
+//     <main>
+//       <div ref={containerRef} className="container">
+//         <div ref={boxRef} className="box"></div>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
