a2ced4f393086152df092be8ef251e3c76856e3c
lvh20895 <levanhau12321@gmail.com>
2023-07-10 18:07:37 +0700

    fix theme

--- client/src/Components/StoryText/StoryText.jsx
+++ client/src/Components/StoryText/StoryText.jsx
@@ -10,6 +10,7 @@ const StoryText = () => {
 	const [content, setContent] = useState("");
 	const [color, setColor] = useState("black");
 	const [background, setBackground] = useState("white");
+	const [theme, setTheme] = useState(0);
 
 	const { colors, backgrounds, themes } = colorful;
 
@@ -23,70 +24,80 @@ const StoryText = () => {
 	return (
 		<div className={style.storyText}>
 			<div className={style.option}>
-				<div className={style.action}>
-					<div className={style.head}>
-						<Link to="/stories" className={style.back}>
-							<FaChevronLeft />
-						</Link>
+				<div className={style.head}>
+					<Link to="/stories" className={style.back}>
+						<FaChevronLeft />
+					</Link>
 
-						<Publish />
+					<Publish />
+				</div>
+
+				<div className={style.group}>
+					<textarea
+						placeholder="Enter your content..."
+						value={content}
+						onChange={handleChangeContent}
+					></textarea>
+					<div className={style.emoji}>
+						<Emoji
+							position={{
+								top: "calc(100% + 15px)",
+								right: "-10px"
+							}}
+							onEmoji={icon => setContent(prev => prev + icon)}
+						/>
 					</div>
+				</div>
 
-					<div className={style.group}>
-						<textarea
-							placeholder="Enter your content..."
-							value={content}
-							onChange={handleChangeContent}
-						></textarea>
-						<div className={style.emoji}>
-							<Emoji
-								position={{
-									top: "calc(100% + 15px)",
-									right: "-10px"
-								}}
-								onEmoji={icon =>
-									setContent(prev => prev + icon)
-								}
-							/>
-						</div>
+				<div className={style.color}>
+					<p className={style.title}>Text color</p>
+					<div className={style.wrap}>
+						{colors.map((clr, i) => {
+							return (
+								<span
+									style={{ background: clr }}
+									className={`${
+										clr === color ? style.active : ""
+									}`}
+									onClick={() => setColor(clr)}
+								></span>
+							);
+						})}
 					</div>
+				</div>
 
-					<div className={style.background}>
-						<p className={style.title}>Background</p>
-						<div className={style.wrap}>
-							{backgrounds.map((bg, i) => {
-								return (
-									<span
-										key={i}
-										style={{ background: bg }}
-										className={`${
-											bg === background
-												? style.active
-												: ""
-										}`}
-										onClick={() => setBackground(bg)}
-									></span>
-								);
-							})}
-						</div>
+				<div className={style.background}>
+					<p className={style.title}>Background content</p>
+					<div className={style.wrap}>
+						{backgrounds.map((bg, i) => {
+							return (
+								<span
+									style={{ background: bg }}
+									className={`${
+										bg === background ? style.active : ""
+									}`}
+									onClick={() => setBackground(bg)}
+								></span>
+							);
+						})}
 					</div>
+				</div>
 
-					<div className={style.color}>
-						<p className={style.title}>Text</p>
-						<div className={style.wrap}>
-							{colors.map((clr, i) => {
-								return (
-									<span
-										key={i}
-										style={{ background: clr }}
-										className={`${
-											clr === color ? style.active : ""
-										}`}
-										onClick={() => setColor(clr)}
-									></span>
-								);
-							})}
-						</div>
+				<div className={style.theme}>
+					<p className={style.title}>Background</p>
+					<div className={style.box}>
+						{themes.map((th, i) => {
+							return (
+								<img
+									src={th.url}
+									alt={th.name}
+									className={`${
+										i === theme ? style.active : ""
+									}`}
+									onClick={() => setTheme(i)}
+								/>
+							);
+						})}
 					</div>
 				</div>
 
@@ -98,7 +109,11 @@ const StoryText = () => {
 			<div className={style.preview}>
 				<div className={style.content}>
 					<p className={style.title}>Preview</p>
-					<div className={style.view}></div>
+					<div className={style.screen}>
+						<div className={style.view}>
+							<img src={themes[theme].url} alt="theme" />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
