3c86a8fcb5ab810047dc9cbc174852162d3e86d2
lvh20895 <levanhau12321@gmail.com>
2023-11-17 17:04:33 +0700

    test 1

--- server/src/Routers/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import userRouter from "./user.router";
-
-const router = Router();
-
-router.use("/user", userRouter);
-
-export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import chalk from "chalk";
 import connectDatabase from "./Configs/connect.database";
-import router from "./Routers";
+// import router from "./Routers";
 
 const app = express();
 const port = 1995;
@@ -10,7 +10,7 @@ const port = 1995;
 app.use(express.json());
 app.use(express.static("."));
 app.use(cors());
-app.use("/api", router);
+// app.use("/api", router);
 
 app.get("/", (_, res) => {
 	res.send("Server running...");
