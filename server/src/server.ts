c9933910256d29d5efa600db955ed497aa030548
lvh20895 <levanhau12321@gmail.com>
2023-11-17 19:06:17 +0700

    test 7

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,12 @@ import router from "./Routers";
 const app = express();
 const port = 1995;
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*"); // Cho phép tất cả các nguồn
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	next();
+});
 app.use(express.json());
 app.use(express.static("."));
 app.use(cors());
