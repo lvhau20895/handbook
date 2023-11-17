b553f5886050b52199db08d4f1149b8cf0e55de6
lvh20895 <levanhau12321@gmail.com>
2023-11-17 18:39:41 +0700

    test 6

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import chalk from "chalk";
 import connectDatabase from "./Configs/connect.database";
-// import router from "./Routers";
+import router from "./Routers";
 
 const app = express();
 const port = 1995;
@@ -10,14 +10,13 @@ const port = 1995;
 app.use(express.json());
 app.use(express.static("."));
 app.use(cors());
-// app.use("/api", router);
+app.use("/api", router);
 
 app.get("/", (_, res) => {
 	res.send("Server running...");
 });
 
-connectDatabase();
-
 app.listen(port, () => {
+	connectDatabase();
 	console.log(chalk.blueBright(`http://localhost:${port}`));
 });
