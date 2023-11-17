d0b4b0ff54f7e9d699c007cb50143d7f4423574f
lvh20895 <levanhau12321@gmail.com>
2023-11-17 19:10:59 +0700

    tets 8

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,15 +7,9 @@ import router from "./Routers";
 const app = express();
 const port = 1995;
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*"); // Cho phép tất cả các nguồn
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	next();
-});
 app.use(express.json());
 app.use(express.static("."));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use("/api", router);
 
 app.get("/", (_, res) => {
